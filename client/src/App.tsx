f5be54f235b4801f7ecbb67db6d066aa49c8116b
Manaus Transez <manatran@student.arteveldehs.be>
2019-07-18 23:39:31 +0200

    feat: redux boilerplate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./store";
+
 import "./App.css";
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			<p>hello world</p>
-		</div>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<Router>
+					<div>hello</div>
+				</Router>
+			</PersistGate>
+		</Provider>
 	);
 };
 
