613cb9c09ddf079ba249eeaa520c9dc4522d9520
Manaus Transez <manatran@student.arteveldehs.be>
2019-07-20 01:17:20 +0200

    feat: create button

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.png";
 import { SearchBar } from "../index";
@@ -7,30 +7,19 @@ import NavigationDropdown from "./NavigationDropdown";
 import "./Navigation.css";
 
 const Navigation: React.FC = () => {
-	// Declare dropdown state variable
-	const [dropdown, setDropdown] = useState(false);
-
 	return (
 		<nav className="row a-centered j-between">
 			<Link to="/" className="logo row centered">
 				<img src={Logo} alt="Orbit" />
 				<span>Orbit</span>
 			</Link>
-			<section className="dropdown">
-				<button
-					onClick={() => setDropdown(!dropdown)}
-					className={`row j-between ${dropdown && "active"}`}
-				>
-					<span className="row a-centered">
-						<i className="material-icons icon">trending_up</i>
-						<span>Feed</span>
-					</span>
-					<i className="material-icons">arrow_drop_down</i>
-				</button>
-				{dropdown && <NavigationDropdown />}
-			</section>
 
+			<NavigationDropdown />
 			<SearchBar />
+
+			<Link to="/submit" className="create centered">
+				<i className="material-icons">create</i>
+			</Link>
 		</nav>
 	);
 };
--- client/src/components/Navigation/NavigationDropdown.tsx
+++ client/src/components/Navigation/NavigationDropdown.tsx
@@ -1,68 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.png";
 
 const NavigationDropdown: React.FC = () => {
+	// Declare dropdown state variable
+	const [dropdown, setDropdown] = useState(false);
+
 	return (
-		<section className="list column">
-			<section className="menu">
-				<h2 className="small">Navigation</h2>
-				<Link to="/challenges" className="row a-centered">
-					<i className="material-icons icon">important_devices</i>
-					Challenges
-				</Link>
-				<Link to="/suggestions" className="row a-centered">
-					<i className="material-icons icon">wb_incandescent</i>
-					Suggestions
-				</Link>
-			</section>
+		<section className="dropdown">
+			<button
+				onClick={() => setDropdown(!dropdown)}
+				className={`row j-between ${dropdown && "active"}`}
+			>
+				<span className="row a-centered">
+					<i className="material-icons icon">trending_up</i>
+					<span>Feed</span>
+				</span>
+				<i className="material-icons">arrow_drop_down</i>
+			</button>
+			{dropdown && (
+				<section className="list column">
+					<section className="menu">
+						<h2 className="small">Navigation</h2>
+						<Link to="/challenges" className="row a-centered">
+							<i className="material-icons icon">important_devices</i>
+							Challenges
+						</Link>
+						<Link to="/suggestions" className="row a-centered">
+							<i className="material-icons icon">wb_incandescent</i>
+							Suggestions
+						</Link>
+					</section>
 
-			<section className="favorites">
-				<h2 className="small">Favorites</h2>
-				<div className="sub row a-centered j-between">
-					<Link to="/challenges" className="row a-centered">
-						<img src={Logo} alt="Challenges" />
-						Subscription
-					</Link>
-					<i
-						className="material-icons fav"
-						onClick={() => console.log("clicked!")}
-					>
-						favorite
-					</i>
-				</div>
-			</section>
+					<section className="favorites">
+						<h2 className="small">Favorites</h2>
+						<div className="sub row a-centered j-between">
+							<Link to="/challenges" className="row a-centered">
+								<img src={Logo} alt="Challenges" />
+								Subscription
+							</Link>
+							<i
+								className="material-icons fav"
+								onClick={() => console.log("clicked!")}
+							>
+								favorite
+							</i>
+						</div>
+					</section>
 
-			<section className="subscriptions">
-				<h2 className="small">Subscriptions</h2>
-				<div className="sub row a-centered j-between">
-					<Link to="/challenges" className="row a-centered">
-						<img src={Logo} alt="Challenges" />
-						Subscription
-					</Link>
-					<i className="material-icons" onClick={() => console.log("clicked!")}>
-						favorite
-					</i>
-				</div>
-				<div className="sub row a-centered j-between">
-					<Link to="/challenges" className="row a-centered">
-						<img src={Logo} alt="Challenges" />
-						Subscription
-					</Link>
-					<i className="material-icons" onClick={() => console.log("clicked!")}>
-						favorite
-					</i>
-				</div>
-				<div className="sub row a-centered j-between">
-					<Link to="/challenges" className="row a-centered">
-						<img src={Logo} alt="Challenges" />
-						Subscription
-					</Link>
-					<i className="material-icons" onClick={() => console.log("clicked!")}>
-						favorite
-					</i>
-				</div>
-			</section>
+					<section className="subscriptions">
+						<h2 className="small">Subscriptions</h2>
+						<div className="sub row a-centered j-between">
+							<Link to="/challenges" className="row a-centered">
+								<img src={Logo} alt="Challenges" />
+								Subscription
+							</Link>
+							<i
+								className="material-icons"
+								onClick={() => console.log("clicked!")}
+							>
+								favorite
+							</i>
+						</div>
+						<div className="sub row a-centered j-between">
+							<Link to="/challenges" className="row a-centered">
+								<img src={Logo} alt="Challenges" />
+								Subscription
+							</Link>
+							<i
+								className="material-icons"
+								onClick={() => console.log("clicked!")}
+							>
+								favorite
+							</i>
+						</div>
+						<div className="sub row a-centered j-between">
+							<Link to="/challenges" className="row a-centered">
+								<img src={Logo} alt="Challenges" />
+								Subscription
+							</Link>
+							<i
+								className="material-icons"
+								onClick={() => console.log("clicked!")}
+							>
+								favorite
+							</i>
+						</div>
+					</section>
+				</section>
+			)}
 		</section>
 	);
 };
