b134ce2c667a2b50ee1ef061768c2f81a93021a7
Manaus Transez <manatran@student.arteveldehs.be>
2019-07-20 00:01:03 +0200

    feat: implemented navigation

---
+++ client/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
+import NavigationDropdown from "./NavigationDropdown";
+
+import "./Navigation.css";
+
+const Navigation: React.FC = () => {
+	// Declare dropdown state variable
+	const [dropdown, setDropdown] = useState(false);
+
+	return (
+		<nav className="row a-centered">
+			<Link to="/" className="logo row centered">
+				<img src={Logo} alt="Orbit" />
+				<span>Orbit</span>
+			</Link>
+			<section className="dropdown">
+				<button
+					onClick={() => setDropdown(!dropdown)}
+					className={`row j-between ${dropdown && "active"}`}
+				>
+					<span className="row a-centered">
+						<i className="material-icons icon">trending_up</i>
+						<span>Feed</span>
+					</span>
+					<i className="material-icons">arrow_drop_down</i>
+				</button>
+
+				{dropdown && <NavigationDropdown />}
+			</section>
+		</nav>
+	);
+};
+export default Navigation;
---
+++ client/src/components/Navigation/NavigationDropdown.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
+
+const NavigationDropdown = () => {
+	return (
+		<section className="list column">
+			<section className="menu">
+				<h2 className="small">Navigation</h2>
+				<Link to="/challenges" className="row a-centered">
+					<i className="material-icons icon">important_devices</i>
+					Challenges
+				</Link>
+				<Link to="/suggestions" className="row a-centered">
+					<i className="material-icons icon">wb_incandescent</i>
+					Suggestions
+				</Link>
+			</section>
+
+			<section className="favorites">
+				<h2 className="small">Favorites</h2>
+				<div className="sub row a-centered j-between">
+					<Link to="/challenges" className="row a-centered">
+						<img src={Logo} alt="Challenges" />
+						Subscription
+					</Link>
+					<i
+						className="material-icons fav"
+						onClick={() => console.log("clicked!")}
+					>
+						favorite
+					</i>
+				</div>
+			</section>
+
+			<section className="subscriptions">
+				<h2 className="small">Subscriptions</h2>
+				<div className="sub row a-centered j-between">
+					<Link to="/challenges" className="row a-centered">
+						<img src={Logo} alt="Challenges" />
+						Subscription
+					</Link>
+					<i className="material-icons" onClick={() => console.log("clicked!")}>
+						favorite
+					</i>
+				</div>
+				<div className="sub row a-centered j-between">
+					<Link to="/challenges" className="row a-centered">
+						<img src={Logo} alt="Challenges" />
+						Subscription
+					</Link>
+					<i className="material-icons" onClick={() => console.log("clicked!")}>
+						favorite
+					</i>
+				</div>
+				<div className="sub row a-centered j-between">
+					<Link to="/challenges" className="row a-centered">
+						<img src={Logo} alt="Challenges" />
+						Subscription
+					</Link>
+					<i className="material-icons" onClick={() => console.log("clicked!")}>
+						favorite
+					</i>
+				</div>
+			</section>
+		</section>
+	);
+};
+export default NavigationDropdown;
