7c65ac835a918ec1aded48ddcf71b2d5265a6784
Manaus Transez <manatran@student.arteveldehs.be>
2019-07-23 18:01:15 +0200

    feat: moved logo to separate component

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import Orbit from "../../assets/logo.png";
 
 const Logo = () => {
-	return <img src={Orbit} alt="Orbit" className="rounded" />;
+	return <img src={Orbit} alt="Orbit" className="rounded logo" />;
 };
 export default Logo;
--- client/src/pages/Home/HomePage.tsx
+++ client/src/pages/Home/HomePage.tsx
@@ -3,12 +3,14 @@ import { Header } from "../../components";
 
 const HomePage: React.FC = () => {
 	return (
-		<div className="body">
+		<>
 			<Header />
-			<main>
-				<p>body</p>
-			</main>
-		</div>
+			<div className="row">
+				<main>
+					<p>body</p>
+				</main>
+			</div>
+		</>
 	);
 };
 export default HomePage;
