08b3212f135a0a8c32cf22d91b3d315b358b1e7c
Manaus Transez <manatran@student.arteveldehs.be>
2019-09-23 18:12:21 +0200

    feat: github pages

---
+++ docs/precache-manifest.b8f85ba2b80e7ec1fa55dc880979b2fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f8e6d07080608864d6b33f8ec42732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "705d3b7132fc09830dd9",
+    "url": "/static/css/main.4251f5ee.chunk.css"
+  },
+  {
+    "revision": "c67ce423b79dfdfbc56a",
+    "url": "/static/js/2.22e489bd.chunk.js"
+  },
+  {
+    "revision": "705d3b7132fc09830dd9",
+    "url": "/static/js/main.52d16db5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4a87f152f2483439170f6b5c61debc4a",
+    "url": "/static/media/logo.4a87f152.png"
+  }
+]);
